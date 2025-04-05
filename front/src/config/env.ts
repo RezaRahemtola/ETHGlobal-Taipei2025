34e13659abe937e1772734d6d243a1634020033b
Reza Rahemtola <reza.rahemtola@epitech.eu>
2025-04-05 22:35:33 +0800

    feat(front): Payment contract var

--- front/src/config/env.ts
+++ front/src/config/env.ts
@@ -3,11 +3,13 @@ import { z } from "zod";
 const envSchema = z.object({
 	BACKEND_API_URL: z.string().url(),
 	THIRDWEB_CLIENT_ID: z.string(),
+	PAYMENT_CONTRACT_ADDRESS: z.string(),
 });
 
 const env = envSchema.parse({
 	BACKEND_API_URL: import.meta.env.VITE_BACKEND_API_URL,
 	THIRDWEB_CLIENT_ID: import.meta.env.VITE_THIRDWEB_CLIENT_ID,
+	PAYMENT_CONTRACT_ADDRESS: import.meta.env.VITE_PAYMENT_CONTRACT_ADDRESS,
 });
 
 export default env;
